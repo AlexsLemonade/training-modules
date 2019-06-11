891e455874895c6d15641bcb4f20a844dbea6183
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-06-11 11:21:18 -0400

    Changes to directories

--- RNA-seq/scripts/run_SRR585570.sh
+++ RNA-seq/scripts/run_SRR585570.sh
@@ -8,7 +8,8 @@
 # FastQC -----------------------------------------------------------------------
 
 # In the interest of time, we'll run one of the fastq files through FastQC
-fastqc data/fastq/gastric_cancer/SRR585570_1.fastq.gz -o QC/gastric_cancer/
+fastqc data/fastq/gastric_cancer/SRR585570/SRR585570_1.fastq.gz \
+	-o data/QC/gastric_cancer/
 
 # Fastp ------------------------------------------------------------------------
 
@@ -16,10 +17,10 @@ fastqc data/fastq/gastric_cancer/SRR585570_1.fastq.gz -o QC/gastric_cancer/
 mkdir fastp_output
 
 # Run the adapter and quality trimming step -- also produces QC report
-fastp -i data/fastq/gastric_cancer/SRR585570_1.fastq.gz \
-	-I data/fastq/gastric_cancer/SRR585570_2.fastq.gz \
-	-o data/fastq/gastric_cancer/SRR585570_fastp_1.fastq.gz \
-	-O data/fastq/gastric_cancer/SRR585570_fastp_2.fastq.gz \
+fastp -i data/fastq/gastric_cancer/SRR585570/SRR585570_1.fastq.gz \
+	-I data/fastq/gastric_cancer/SRR585570/SRR585570_2.fastq.gz \
+	-o data/fastq/gastric_cancer/SRR585570/SRR585570_fastp_1.fastq.gz \
+	-O data/fastq/gastric_cancer/SRR585570/SRR585570_fastp_2.fastq.gz \
 	--qualified_quality_phred 15 \
 	--length_required 20 \
 	--report_title "SRR585570" \
@@ -33,8 +34,8 @@ fastp -i data/fastq/gastric_cancer/SRR585570_1.fastq.gz \
 # per the Salmon documentation
 salmon quant -i index/Homo_sapiens/short_index \
 	-l A \
-    -1 data/fastq/gastric_cancer/SRR585570_fastp_1.fastq.gz \
-    -2 data/fastq/gastric_cancer/SRR585570_fastp_2.fastq.gz \
+    -1 data/fastq/gastric_cancer/SRR585570/SRR585570_fastp_1.fastq.gz \
+    -2 data/fastq/gastric_cancer/SRR585570/SRR585570_fastp_2.fastq.gz \
     -o data/quant/gastric_cancer/SRR585570 \
     --validateMappings --rangeFactorization 4 \
 	--gcBias --seqBias
