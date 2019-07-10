56381ac7206256393624519239e5b752bce994c5
Candace Savonen <savonenc@msu.edu>
2019-07-10 11:09:25 -0400

    Use file.path

--- ready_script.R
+++ ready_script.R
@@ -6,10 +6,10 @@
 # This script is meant to be run in a training workshop to test if everything is
 # set up.
 
-# Usage: source("kitematic/ready_script.R")
+# Usage: source(file.path("kitematic", "ready_script.R"))
 
 # Read in the image
-image <- png::readPNG("kitematic/ready.png")
+image <- png::readPNG(file.path("kitematic", "ready.png"))
 
 # Raster the image
 grid::grid.raster(image)
