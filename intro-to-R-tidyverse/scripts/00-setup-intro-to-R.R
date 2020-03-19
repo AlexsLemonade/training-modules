6840d22d59df30cbf5583b0200c8542058c6b186
Candace Savonen <cansav09@gmail.com>
2020-03-19 13:19:39 -0400

    Add 2x2 design results temporarily but might revert

--- intro-to-R-tidyverse/scripts/00-setup-intro-to-R.R
+++ intro-to-R-tidyverse/scripts/00-setup-intro-to-R.R
@@ -45,44 +45,83 @@ gene_df <- readr::read_tsv(file.path(input_dir,
 # Read in metadata
 metadata <- readr::read_tsv(file.path(input_dir, 
                                       "metadata_GSE44971.tsv")) %>%
-  dplyr::select(-which(apply(is.na(.), 2, all))) %>% 
-  dplyr::mutate(tissue = as.factor(tissue))
+  dplyr::select(-which(apply(is.na(.), 2, all)), 
+                sex = refinebio_sex) %>% 
+  dplyr::mutate(
+    # Clean up character names
+    tissue = gsub(" ", "_", tolower(tissue)),
+    # Turn our testing variables in factors
+    #tissue = as.factor(tissue),
+    #sex = as.factor(sex)
+    )
 
 ## Put samples' data and metadata in the same order
 # Make the data in the order of the metadata
 gene_df <- gene_df %>% 
   dplyr::select(metadata$geo_accession)
 
+#This set up is based on the example 11.4 Estrogen Data: A 2x2 Factorial Experiment in 
+# https://bioc.ism.ac.jp/packages/2.9/bioc/vignettes/limma/inst/doc/usersguide.pdf
+ 
+# Set up combined sex and tissue variable for model
+sex_tissue <- as.factor(paste0(metadata$tissue, "_", metadata$sex))
+
+# Set variable levels
+sex_tissue = factor(sex_tissue,
+                    labels = c("control_female", 
+                               "control_male", 
+                               "tumor_female", 
+                               "tumor_male")
+                    )
+
+# Set up contrasts
+contrasts(sex_tissue) <- cbind(sex = c(0, 1, 0, 1), 
+                               tumor_male = c(0, 0, 0, 1),
+                               tumor_female = c(0, 0, 1, 0))
+
 # Create the design matrix from the genotype information
-des_mat <- model.matrix(~metadata$tissue)
+des_mat <- model.matrix(~ sex_tissue)
+
+# Neaten up column names
+colnames(des_mat) <- c("intercept", "sex", "tumor_male", "tumor_female")
 
 # Apply linear model to data
 fit <- limma::lmFit(gene_df, design = des_mat)
 
+# Set up a contrast matrix that focuses on tumors
+contrast_mat <- cbind(tumor_male = c(0, 0, 0, 1),
+                      tumor_female = c(0, 0, 1, 0))
+
+# Do final fitting
+fit2 <- contrasts.fit(fit, contrast_mat)
+
 # Apply empirical Bayes to smooth standard errors
-fit <- limma::eBayes(fit)
+fit2 <- eBayes(fit2)
 
 # Apply multiple testing correction and obtain stats
-stats_df <- limma::topTable(fit, number = nrow(gene_df)) %>% 
+stats_df <- limma::topTable(fit2, number = nrow(gene_df)) %>% 
   # Move ensembl IDs to their own column
-  tibble::rownames_to_column("ensembl_ids") %>%
+  tibble::rownames_to_column("ensembl_id") %>%
   # Map ensembl IDs to their associated gene symbols
-  dplyr::mutate("gene_symbols" = AnnotationDbi::mapIds(
+  dplyr::mutate("gene_symbol" = AnnotationDbi::mapIds(
     org.Hs.eg.db::org.Hs.eg.db, 
-    keys = ensembl_ids, 
+    keys = ensembl_id, 
     column = "SYMBOL", 
     keytype = "ENSEMBL")) %>%  
   # Clean up column names and reorder 
   dplyr::select(
-    ensembl_ids, 
-    gene_symbols,
+    ensembl_id, 
+    gene_symbol,
+    tumor_female, 
+    tumor_male, 
     avg_expression = AveExpr,
-    log_fold_change = logFC, 
-    t_value = t, 
+    #log_fold_change = logFC, 
+    #t_value = t, 
     p_value = P.Value, 
     adj_p_value = adj.P.Val, 
-    log_odds = B
+    #log_odds = B
     ) %>% 
   # Write this to TSV
   readr::write_tsv(file.path(data_dir, "gene_results_GSE44971.tsv"))
 
+colnames(stats_df)
