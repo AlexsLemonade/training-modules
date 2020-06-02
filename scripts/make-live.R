249888fd1ab0df9752f1f0a65c592ae2eb611eca
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-06-02 15:13:25 -0400

    Add comments to make-live.R

--- scripts/make-live.R
+++ scripts/make-live.R
@@ -1,20 +1,25 @@
 #!/usr/bin/env Rscript
 #
 # Make live versions of .Rmd files in training modules
+#
+# Replaces code in chunks with a chunk option of `live = TRUE`
+# Comments are preserved
 
 # Install exrcise package if needed.
 if (!"exrcise" %in% installed.packages()){
   remotes::install_github("AlexsLemonade/exrcise", dependencies = TRUE)
 }
 
+# find the project root
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
+# list of files to transform
 infiles <- c(file.path(root_dir, "intro-to-R-tidyverse", 
                        c("01-intro_to_base_R.Rmd", 
                          "02-intro_to_ggplot2.Rmd",
                          "03-intro_to_tidyverse.Rmd"))
 )
-
+# new files will be made with -live.Rmd suffix
 outfiles <- stringr::str_replace(infiles, "(.*)\\.Rmd$", "\\1-live.Rmd")
 
 # capture to avoid printing to stdout
