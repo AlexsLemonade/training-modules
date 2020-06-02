f3e36dc0e51722e4631f093e69c4ebeeec0d1d72
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-06-02 13:38:38 -0400

    Add script to generate -live files

---
+++ scripts/make-live.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+#
+# Make live versions of Rmd files.
+
+# Install exrcise package
+if (!"exrcise" %in% installed.packages()){
+  remotes::install_github("AlexsLemonade/exrcise", dependencies = TRUE)
+}
+
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+
+infiles <- c(file.path(root_dir, "intro-to-R-tidyverse", 
+                       c("01-intro_to_base_R.Rmd", 
+                         "02-intro_to_ggplot2.Rmd",
+                         "03-intro_to_tidyverse.Rmd"))
+)
+
+outfiles <- stringr::str_replace(infiles, "(.*)\\.Rmd$", "\\1-live.Rmd")
+
+purrr::map2(infiles, outfiles, exrcise, replace_flags = "live")
