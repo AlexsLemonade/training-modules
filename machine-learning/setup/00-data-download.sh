32aa56b0e82f1ccaf246072e84c7342135d7af0a
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-07-21 01:10:29 +0000

    @jashapiro suggestions

--- machine-learning/setup/00-data-download.sh
+++ machine-learning/setup/00-data-download.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Adapted from:
+# https://github.com/AlexsLemonade/OpenPBTA-analysis/blob/d23b5f07f6eadeeadf7a5061a374edf17e6bf279/download-data.sh
 
 set -e
 set -o pipefail
@@ -6,16 +8,18 @@ set -o pipefail
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# Use the OpenPBTA bucket as the default.
-URL=${OPENPBTA_URL:-https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data}
+# Use the most recent release, as of writing this script, as the default
 RELEASE=${OPENPBTA_RELEASE:-release-v16-20200320}
+
+# Use the OpenPBTA bucket.
+bucket_url=https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data
 
 # All of the OpenPBTA data will be located in data/open-pbta/download folder
 # (from root of module directory) - we won't include the release in the folder structure
 data_dir=../data/open-pbta/download
 
 # The md5sum file provides our single point of truth for which files are in a release.
-curl --create-dirs $URL/$RELEASE/md5sum.txt -o ${data_dir}/md5sum.txt -z ${data_dir}/md5sum.txt
+curl --create-dirs $bucket_url/$RELEASE/md5sum.txt -o ${data_dir}/md5sum.txt ${data_dir}/md5sum.txt
 
 # Consider the filenames in the md5sum file and the release notes
 FILES=(`tr -s ' ' < ${data_dir}/md5sum.txt | cut -d ' ' -f 2` release-notes.md)
@@ -23,7 +27,7 @@ FILES=(`tr -s ' ' < ${data_dir}/md5sum.txt | cut -d ' ' -f 2` release-notes.md)
 # Download the items in FILES if newer than what's on server
 for file in "${FILES[@]}"
 do
-  curl --create-dirs $URL/$RELEASE/$file -o ${data_dir}/$file -z ${data_dir}/$file
+  curl --create-dirs $bucket_url/$RELEASE/$file -o ${data_dir}/$file ${data_dir}/$file
 done
 
 cd ${data_dir}
