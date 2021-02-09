1d397acaee46ddd3b923a4f27da65ec103000a41
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-02-09 13:13:18 +0000

    Update config files and scripts to make configfile specification mandatory.

--- RNA-seq/setup/setup.sh
+++ RNA-seq/setup/setup.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-set -euo pipefail
+# This script runs two snakemake workflows to populate the 
+# `/shared/training-modules/RNA-seq` directory with preprocessed data
+# for the notebooks in that module
 
-jobs=8
+
+# safety flags
+set -euo pipefail
+# Set the working directory to the directory of this file
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+jobs=12
 
 # create gastric cancer files (keep fastq)
-snakemake -j ${jobs} --notemp
+snakemake -j ${jobs} --notemp --configfile config-GC.yaml
 
-# create NB cell line files(fastq will be deleted)
+# create NB cell line files (fastq will be deleted)
 snakemake -j ${jobs} --configfile config-NB.yaml
