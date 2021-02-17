a08d994a172ae358827eeac7d6365c4cc4db08da
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-02-17 22:11:47 +0000

    separate directories and files

--- scripts/syncup-s3.sh
+++ scripts/syncup-s3.sh
@@ -2,31 +2,45 @@
 
 # This script is used to sync files required for training module notebooks to S3
 
+set -euo pipefail
+
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 # move back up to the training modules root
 cd ..
 
-bucket=s3://ccdatalab-training-data/training-modules
+bucket=s3://ccdl-training-data/training-modules
 
-sync_locs=(
+sync_dirs=(
   intro-to-R-tidyverse/data
-  RNA-seq/data/gastric-cancer/gastric-cancer_metadata.tsv
   RNA-seq/data/gastric-cancer/salmon_quant
-  RNA-seq/data/NB-cell/NB-cell_metadata.tsv
   RNA-seq/data/NB-cell/tximport
+  scRNA-seq/data/tabula-muris/alevin-quant/10X_P4_3
+  scRNA-seq/index/Mus_musculus
+  machine-learning/data/open-pbta/processed
+)
+
+sync_files=(
+  RNA-seq/data/gastric-cancer/gastric-cancer_metadata.tsv
+  RNA-seq/data/NB-cell/NB-cell_metadata.tsv
   RNA-seq/index/Homo_sapiens/Homo_sapiens.GRCh38.95_tx2gene.tsv
   scRNA-seq/data/glioblastoma/preprocessed/tximport/count_matrix.tsv
   scRNA-seq/data/glioblastoma/preprocessed/darmanis_metadata.tsv
-  scRNA-seq/data/tabula-muris/alevin-quant/10X_P4_3
   scRNA-seq/data/tabula-muris/normalized/TM_normalized.rds
-  scRNA-seq/index/Mus_musculus
-  machine-learning/data/open-pbta/processed
   pathway-analysis/data/medulloblastoma/medulloblastoma_vst_collapsed.tsv
 )
 
-for loc in ${link_locs[@]}
+for loc in ${sync_dirs[@]}
 do
   # upload to S3, make public, ignore timestamps, ignore hidden files
-  aws s3 sync ${loc} ${bucket}/${loc} --acl public-read --size-only --exclude ".*"
+  aws s3 sync ${loc} ${bucket}/${loc} \
+   --acl public-read --size-only \
+   --exclude ".*"
+done
+
+for loc in ${sync_files[@]}
+do
+  # upload to S3, make public
+  aws s3 cp ${loc} ${bucket}/${loc} \
+   --acl public-read
 done
