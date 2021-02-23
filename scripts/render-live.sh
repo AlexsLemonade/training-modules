9f83027e3000cdd9c0ce9461c8a5aafaccd38a00
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-02-23 17:37:53 -0500

    Let's try PLIER

--- scripts/render-live.sh
+++ scripts/render-live.sh
@@ -28,8 +28,8 @@ files=(
   scRNA-seq/05-dimension_reduction_scRNA-seq.Rmd
   machine-learning/01-openpbta_heatmap.Rmd
   machine-learning/02-openpbta_consensus_clustering.Rmd
-  #  machine-learning/03-openpbta_PLIER.Rmd
-  #  machine-learning/04-openpbta_plot_LV.Rmd
+  machine-learning/03-openpbta_PLIER.Rmd
+  machine-learning/04-openpbta_plot_LV.Rmd
   pathway-analysis/01-overrepresentation_analysis.Rmd
   pathway-analysis/02-gene_set_enrichment_analysis.Rmd
   pathway-analysis/03-gene_set_variation_analysis.Rmd
