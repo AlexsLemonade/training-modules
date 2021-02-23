88f8ad3fb5bc34ce0d8a264b8a202fb7066e8b16
cansavvy <cansav09@gmail.com>
2021-02-23 19:48:45 +0000

    update comments

--- RNA-seq/setup/setup.sh
+++ RNA-seq/setup/setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# This script runs two snakemake workflows to populate the
+# This script runs three snakemake workflows to populate the
 # `/shared/training-modules/RNA-seq` directory with preprocessed data
 # for the notebooks in that module
 
 
 # safety flags
 set -euo pipefail
+
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
@@ -18,5 +19,5 @@ snakemake -j ${jobs} --notemp --configfile config-GC.yaml
 # create NB cell line files (fastq will be deleted)
 snakemake -j ${jobs} --configfile config-NB.yaml
 
-# create NB cell line files (fastq will be deleted)
+# create zebrafish cortisol files (fastq will be deleted)
 snakemake -j ${jobs} --configfile config-zebrafish.yaml
