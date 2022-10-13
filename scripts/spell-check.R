7e13c360322797eeeeb2a2627f8a6cdb166dcd6d
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2022-10-13 17:54:53 -0400

    Revert "reduce  spellcheck dependencies"
    This reverts commit badf2a23ff92d5d254523ed1f8fc2d3c3353b531.

--- scripts/spell-check.R
+++ scripts/spell-check.R
@@ -3,6 +3,8 @@
 # Run spell check and save results
 # Adapted from: https://github.com/AlexsLemonade/refinebio-examples/blob/33cdeff66d57f9fe8ee4fcb5156aea4ac2dce07f/scripts/spell-check.R
 
+library(magrittr)
+
 # Find .git root directory
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
@@ -19,7 +21,7 @@ files <- list.files(pattern = '\\.(Rmd|md)$', recursive = TRUE, full.names = TRU
 # remove files in renv library
 files <- grep('^./renv/', files, invert = TRUE, value = TRUE)
 
-# Remove the LICENSE from the spell check
+# Remove the LICENSE from the spell check 
 files <- grep('LICENSE.md', files, invert = TRUE, value = TRUE)
 
 # Remove all live versions so we're not spell checking twice
@@ -28,13 +30,13 @@ files <- grep('-live.Rmd', files, invert = TRUE, value = TRUE)
 
 
 # Run spell check
-spelling_errors <- spelling::spell_check_files(files, ignore = dictionary) |>
-  data.frame() |>
-  tidyr::unnest(cols = found) |>
+spelling_errors <- spelling::spell_check_files(files, ignore = dictionary) %>%
+  data.frame() %>%
+  tidyr::unnest(cols = found) %>%
   tidyr::separate(found, into = c("file", "lines"), sep = ":")
 
 # Print out how many spell check errors
 write(nrow(spelling_errors), stdout())
 
 # Save spell errors to file temporarily
-write.table(spelling_errors, 'spell_check_errors.tsv', sep = "\t")
+readr::write_tsv(spelling_errors, 'spell_check_errors.tsv')
