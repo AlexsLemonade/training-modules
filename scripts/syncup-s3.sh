231d7cb1a1b3b3767f518f99707b7b41be7a9f55
Ally Hawkins <ally.hawkins@ccdatalab.org>
2026-08-05 12:35:14 -0500

    make sure files are in files list not dirs list

--- scripts/syncup-s3.sh
+++ scripts/syncup-s3.sh
@@ -62,8 +62,6 @@ sync_dirs=(
   spatial/data/brca-xenium/xenium
   spatial/data/crc-hd/visium_hd
   spatial/data/crc-hd/single_cell
-  spatial/data/crc-hd/crc_hd_normalized_spe.rds
-  spatial/data/crc-hd/crc_single_cell_normalized_sce.rds
   machine-learning/data/open-pbta/processed
   pathway-analysis/data/leukemia
   pathway-analysis/data/medulloblastoma
@@ -85,6 +83,8 @@ sync_files=(
   spatial/data/osteo/GSM8478586/normalized/osteo_normalized_spe.rds
   spatial/data/crc-v1/normalized/crc_normalized_spe.rds
   spatial/data/brca-xenium/Janesick_annotations.tsv
+  spatial/data/crc-hd/crc_hd_normalized_spe.rds
+  spatial/data/crc-hd/crc_single_cell_normalized_sce.rds
 )
 
 output_files=(
